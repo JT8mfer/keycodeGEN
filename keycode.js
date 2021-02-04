0623d198dbdb42f57c1ac62236e33d14bd2757b4
jordan <jthomasw88>
2021-02-04 14:48:42 +0000

    #2 key-code challenge complete

--- keycode.js
+++ keycode.js
@@ -1,4 +1,25 @@
+// const code = document.getElementsByid("code");
+
+// code.addEventListener("keydown",(event){
+//   div.textContent = input.value;
+// });
+
 window.addEventListener("keydown", function (event) {
-  let h1 = document.getElementById("set");
-  h1.textContent = `key pressed='${event.key}' | code='${event.code}'`;
+  let h1 = document.getElementById("which");
+  h1.textContent = `${event.which}`;
+});
+
+window.addEventListener("keydown", function (event) {
+  let div = document.getElementById("key");
+  div.textContent = `Key: ${event.key}`;
+});
+
+window.addEventListener("keydown", function (event) {
+  let div = document.getElementById("code");
+  div.textContent = `Code: ${event.code}`;
+});
+
+window.addEventListener("keydown", function (event) {
+  let div = document.getElementById("nums");
+  div.textContent = `which: ${event.which}`;
 });
